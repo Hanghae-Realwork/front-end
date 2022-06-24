bc4a0646ff6966e2dd6cb3bebf650ec273291320
Frost <tolerate0407@gmail.com>
2022-06-24 17:30:25 +0900

    리액트 및 라이브러리 기본 골격 구축 완료

---
+++ real_project/src/redux/configstore.js
@@ -0,0 +1,18 @@
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
+
+// middlewares
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+// reducers
+import user from "./modules/user";
+
+
+const rootReducer = combineReducers({ user });
+
+// logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
+const middlewares = [thunk, logger];
+
+const store = legacy_createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
