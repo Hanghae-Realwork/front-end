e43a353d0617d71fb4dd3c9d34f69698b4040f15
kimjinhee <kjh11071@gmail.com>
2022-06-25 15:30:57 +0900

    mainn 뼈대 설정중

--- real_project/src/App.js
+++ real_project/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+
+//Pages 연결
+import Main from "./pages/Main";
+
 
 function App() {
   return (
     <div className="App">
-      <main/>
-      <test/>
-    </div>
+
+      <Route>
+        <Route path="/" element={<Main />} />
+        <main />
+        <test />
+      </Route>
+    </div >
   );
 }
 
--- real_project/src/redux/configstore.js
+++ real_project/src/redux/configstore.js
@@ -2,16 +2,18 @@ import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 
 // middlewares
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 // reducers
 import user from "./modules/user";
 
 
-const rootReducer = combineReducers({ user });
+const rootReducer = combineReducers({ 
+    user 
+});
 
 // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
 
 const store = legacy_createStore(rootReducer, applyMiddleware(...middlewares));
 
