e0197d2857b83fabd8ddb75f748d96e60d11d71a
kjh11071 <105156100+kjh11071@users.noreply.github.com>
2022-06-27 11:57:45 +0900

    Update configstore.js
    configstore.js 수정

--- real_project/src/redux/configstore.js
+++ real_project/src/redux/configstore.js
@@ -2,7 +2,7 @@ import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 
 // middlewares
 import thunk from "redux-thunk";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 // reducers
 import user from "./modules/user";
@@ -13,7 +13,7 @@ const rootReducer = combineReducers({
 });
 
 // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
-const middlewares = [thunk];
+const middlewares = [thunk, logger];
 
 const store = legacy_createStore(rootReducer, applyMiddleware(...middlewares));
 
