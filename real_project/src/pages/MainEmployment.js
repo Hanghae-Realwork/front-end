030e9fbe20831c064348c88a196f2efe64bb394f
kimjinhee <kjh11071@gmail.com>
2022-06-27 11:44:48 +0900

    메인 수정중

--- real_project/src/pages/MainEmployment.js
+++ real_project/src/pages/MainEmployment.js
@@ -1,34 +1,44 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
+
 
 const MainEmployment = () => {
-    <>
-    </>
+    const  navigate =useNavigate
+    
     return (
         <>
-            <p>메인 구직 페이지 입니다.</p>
+            <p>메인 팀원찾기 페이지 입니다.</p>
             {/* 전체적인 내용 완성 후 styled 방식으로 변경 */}
             {/* Header이후의 내용으로 작성 */}
             <div className="Werp">
                 {/* 헤더 생성 전 임시로 로그인 로그아웃 버튼 생성 */}
                 {/* 각 기능 활성화시 사용 가능하도록 */}
                 <div className="BtnBox">
-                    <loginbox>
                         <button className="LoginBtn"
                         // onClick={() => {
                         //     navigate("/Login")
                         // }}
                         >로그인</button>
-                    </loginbox>
-                    <signupbox>
                         <button className="JoinBtn"
                         // onClick={(
                         //     navigate("/Join")
                         // )}
                         >회원가입</button>
-                    </signupbox>
-                    <logoutbox>
                         <button className="LogoutBtn">로그아웃</button>
-                    </logoutbox>
+                    <br/>
+                        {/* replace용도로도 좋고 새로고침이나 정보를 다시 받을 수 있는 용도 */}
+                        <button className="RecruitBtn"
+                        onClick={(
+                            navigate("/")
+                        )}
+                        >프로젝트</button>
+                        {/* 팀원찾기 페이지로 이동 */}
+                        <button className="EmploymentBtn"
+                        onClick={(
+                            navigate("/MainEmployment")
+                        )}
+                        >팀원찾기</button>
+                    
                 </div>
                 <div className="RoleSelectBox">
                     {/* 직군 선택을 위한 드롭박스(?)/ 필터 */}
