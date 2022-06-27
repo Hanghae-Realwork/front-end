b181eb5d993e60d1cfdab3f14a96b148b184cefe
qoqomi <energyy044@gmail.com>
2022-06-27 12:54:48 +0900

    app.js

--- real_project/src/App.js
+++ real_project/src/App.js
@@ -1,40 +1,38 @@
-import styled from "styled-components"
-import { Routes, Route} from "react-router-dom";
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
 
-import Addprofile from "./pages/AddProfile"
-import Chat from "./pages/Chat"
-import EmploymentProfile from "./pages/EmploymentProfile"
-import Join from "./pages/Join"
-import Login from "./pages/Login"
-import MainEmployment from "./pages/MainEmployment"
-import MainRecruit from "./pages/MainRecruit"
-import Recruit from "./pages/Recruit"
-import RecruitWrite from "./pages/RecruitWrite"
-
-
+import Addprofile from "./pages/AddProfile";
+import Chat from "./pages/Chat";
+import EmploymentProfile from "./pages/EmploymentProfile";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import MainEmployment from "./pages/MainEmployment";
+import MainRecruit from "./pages/MainRecruit";
+import Recruit from "./pages/Recruit";
+import RecruitWrite from "./pages/RecruitWrite";
 
 function App() {
   return (
     <MainWrap>
-      <Addprofile/>
-      <Chat/>
-      <EmploymentProfile/>
-      <Join/>
-      <Login/>
-      <MainEmployment/>
-      <MainRecruit/>
-      <Recruit/>
-      <RecruitWrite/>
+      {/* <Addprofile />
+      <Chat />
+      <EmploymentProfile />
+      <Join />
+      <Login />
+      <MainEmployment />
+      <MainRecruit />
+      <Recruit />
+      <RecruitWrite /> */}
       <Routes>
-            <Route path="/" element={<MainRecruit />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/employmentprofile" element={<EmploymentProfile />} />
-            <Route path="/addprofile" element={<Addprofile />}/>
-            <Route path="/mainemployment" element={<MainEmployment />}></Route>
-            <Route path="/recruit" element={<Recruit />}></Route>
-            <Route path="/recruitwrite" element={<RecruitWrite />}></Route>
-            <Route path="/chat" element={<Chat />}></Route>
+        <Route path="/" element={<MainRecruit />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/employmentprofile" element={<EmploymentProfile />} />
+        <Route path="/addprofile" element={<Addprofile />} />
+        <Route path="/mainemployment" element={<MainEmployment />}></Route>
+        <Route path="/recruit" element={<Recruit />}></Route>
+        <Route path="/recruitwrite" element={<RecruitWrite />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
       </Routes>
     </MainWrap>
   );
@@ -45,7 +43,6 @@ const MainWrap = styled.div`
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
-`
-
+`;
 
 export default App;
