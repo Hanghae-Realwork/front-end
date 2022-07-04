9c7689bc3cde6ddfbb76f97ce6bd233af2acb33b
Frost <tolerate0407@gmail.com>
2022-07-04 21:41:12 +0900

    메인 문구 및 CSS 수정

--- real_project/src/components/DayPickerMain.js
+++ real_project/src/components/DayPickerMain.js
@@ -8,6 +8,7 @@ import "./day-picker.css";
 
 import up from "../image/up-fill.svg";
 import down from "../image/down-fill.svg";
+
 const DayPickerMain = () => {
   const [selected, setSelected] = useState("");
   const today = new Date();
@@ -19,34 +20,20 @@ const DayPickerMain = () => {
   };
 
   let footer = (
-    <Container1
-      type="button"
-      className="pa2 bg-white button-reset ba"
-      aria-label="Pick a date"
-      onClick={onClickToggle}
-    >
+    <Container1 className="pa2 bg-white button-reset ba" aria-label="Pick a date" onClick={onClickToggle}>
       <ContainerStartDateDiv1>
-        <ButtonName>프로젝트 시작</ButtonName>
-        <SelectedDate>날짜 입력</SelectedDate>
-      </ContainerStartDateDiv1>
-      <ContainerStartDateDiv1>
-        <ButtonName>프로젝트 끝</ButtonName>
-        <SelectedDate>날짜 입력</SelectedDate>
+        <ButtonName>프로젝트 기간</ButtonName>  <img src = {down} />
+        {/* <SelectedDate>날짜 입력</SelectedDate> */}
       </ContainerStartDateDiv1>
     </Container1>
   );
   if (selected?.from) {
     if (!selected.to) {
       footer = (
-        <Container1
-          type="button"
-          className="pa2 bg-white button-reset ba"
-          aria-label="Pick a date"
-          onClick={onClickToggle}
-        >
+        <Container1 type="button" className="pa2 bg-white button-reset ba"
+          aria-label="Pick a date" onClick={onClickToggle}>
           <ContainerStartDateDiv1>
-            <ButtonName>프로젝트 시작</ButtonName>
-            <SelectedDate>{format(selected.from, "MM월 dd일 ")}</SelectedDate>
+            <ButtonName>프로젝트 시작<SelectedDate>{format(selected.from, "MM월 dd일 ")}</SelectedDate></ButtonName>
           </ContainerStartDateDiv1>
           <ContainerStartDateDiv1>
             <ButtonName>프로젝트 끝</ButtonName>
@@ -56,12 +43,8 @@ const DayPickerMain = () => {
       );
     } else if (selected?.to) {
       footer = (
-        <Container1
-          type="button"
-          className="pa2 bg-white button-reset ba"
-          aria-label="Pick a date"
-          onClick={onClickToggle}
-        >
+        <Container1 type="button" className="pa2 bg-white button-reset ba"
+          aria-label="Pick a date" onClick={onClickToggle}>
           <ContainerStartDateDiv1>
             <ButtonName>프로젝트 시작</ButtonName>
             <SelectedDate>{format(selected.from, "MM월 dd일 ")}</SelectedDate>
@@ -79,28 +62,23 @@ const DayPickerMain = () => {
     <DivDayPicker>
       {footer}
       {toggle ? (
-        <DayPicker
-          className="dayPicker_container__divMain"
-          styles={{
-            caption: { fontSize: "10px", padding: "10px" },
-          }}
+        <DayPicker className="dayPicker_container__divMain"
+          styles={{caption: { fontSize: "13px", padding: "10px", color: "#AE97E3" }}}
           mode="range"
           selected={selected}
           onSelect={setSelected}
           locale={ko}
           numberOfMonths={2}
-          disabled={{ before: today }}
-        ></DayPicker>
-      ) : (
-        ""
-      )}
+          disabled={{ before: today }}></DayPicker>
+      ) : ("")}
     </DivDayPicker>
   );
 };
 
 //제일 큰 상자
-const Container1 = styled.label`
+const Container1 = styled.div`
   display: flex;
+  flex-flow: row wrap;
   justify-content: center;
   align-items: center;
   outline: none;
@@ -109,29 +87,36 @@ const Container1 = styled.label`
   border-top-left-radius: 10px;
   font-weight: bold;
   color: white;
+  cursor: pointer;
+  /* border: 1px solid black; */
+  width: 170px;
 `;
+
 //시작날짜, 끝날짜 따로따로
 const ContainerStartDateDiv1 = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
-  align-items: flex-start;
-  cursor: pointer;
-  border-radius: 20px;
-  padding-left: 10px;
-  padding-top: 5px;
-  padding-bottom: 5px;
+  flex-flow: row wrap;
+  align-items: center;
+  /* padding: 5px; */
+  /* border: 1px solid black; */
 `;
+
 //시작날짜 폰트
-const ButtonName = styled.p`
-  margin: 0 0 4px 2px;
-  font-size: 12px;
-  font-weight: 600;
+const ButtonName = styled.span`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 10px;
 `;
+
 //선택날짜 폰트
-const SelectedDate = styled.div`
+const SelectedDate = styled.span`
   min-width: 110px;
-  font-size: 12px;
+  font-size: 13px;
 `;
 
 //전체
@@ -143,6 +128,6 @@ const DivDayPicker = styled.div`
   position: relative;
 `;
 
-//전체
+
 
 export default DayPickerMain;
