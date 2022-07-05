9d432b8b05a2e6fc1de104aab6c32f80f76b7d30
qoqomi <energyy044@gmail.com>
2022-07-05 18:46:58 +0900

    삭제

--- real_project/src/pages/RecruitWrite.js
+++ real_project/src/pages/RecruitWrite.js
@@ -1,153 +1,248 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import DayPickerSub from "../components/DayPickerSub";
-
-import Plus from "../image/plus.svg";
-
+import { useForm } from "react-hook-form";
 function RecruitWrite() {
+  const [role, setRole] = useState("");
+
+  const { handleSubmit, register, error, watch } = useForm({
+    defaultValues: {
+      title: "",
+      details: "",
+      subscript: "",
+      role: "",
+      start: "",
+      end: "",
+      skills: [],
+      email: "",
+      phone: "",
+      schedule: null,
+    },
+  });
+
+  console.log(watch());
+  const saveBtnClick = () => {};
   return (
     <>
       <RecruitWriteWrap>
-        <RecruitWriteTopWrap>
+        <form onSubmit={handleSubmit(saveBtnClick)}>
+          <RecruitWriteTopWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>구하는 직군</RecTitleText>
+            </RecTitleTextWrap>
+            <RecTopTextContentWrap>
+              <RecTopRadioLabel>
+                <RecTopRadio
+                  type="radio"
+                  name="RecRadio"
+                  value="frontend"
+                  {...register("role")}
+                />
+                프론트엔드 개발자
+              </RecTopRadioLabel>
+              <RecTopRadioLabel>
+                <RecTopRadio
+                  type="radio"
+                  name="RecRadio"
+                  value="backend"
+                  {...register("role")}
+                />
+                백엔드 개발자
+              </RecTopRadioLabel>
+              <RecTopRadioLabel>
+                <RecTopRadio
+                  type="radio"
+                  name="RecRadio"
+                  value="designer"
+                  {...register("role")}
+                />
+                그래픽 디자이너
+              </RecTopRadioLabel>
+            </RecTopTextContentWrap>
+          </RecruitWriteTopWrap>
+
+          <RecruitWriteMidWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>요구 스킬</RecTitleText>
+            </RecTitleTextWrap>
+            <RecMidContentWrap>
+              <MidTextWrap>
+                <MidText>개발자</MidText>
+              </MidTextWrap>
+              <MidContetWrap>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="React"
+                    {...register("skills")}
+                  />
+                  React
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Vue.js"
+                    {...register("skills")}
+                  />
+                  Vue.js
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="JavaScript"
+                    {...register("skills")}
+                  />
+                  JavaScript
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Node.js"
+                    {...register("skills")}
+                  />
+                  Node.js
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Java"
+                    {...register("skills")}
+                  />
+                  Java
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Spring"
+                    {...register("skills")}
+                  />
+                  Spring
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Python"
+                    {...register("skills")}
+                  />
+                  Python
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="C"
+                    {...register("skills")}
+                  />
+                  C
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="C++"
+                    {...register("skills")}
+                  />
+                  C++
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="C#"
+                    {...register("skills")}
+                  />
+                  C#
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Swift"
+                    {...register("skills")}
+                  />
+                  Swift
+                </ConLabel>
+                <ConLabel>
+                  <InputCon
+                    id="skills"
+                    type="checkbox"
+                    vlaue="Kotlin"
+                    {...register("skills")}
+                  />
+                  Kotlin
+                </ConLabel>
+              </MidContetWrap>
+              <MidTextWrap>
+                <MidText>디자이너</MidText>
+              </MidTextWrap>
+              <MidContetWrap>
+                <ConLabel>
+                  <InputCon type="checkbox" />
+                  Illustrator
+                </ConLabel>
+                <ConLabel>
+                  <InputCon type="checkbox" />
+                  adobe XD
+                </ConLabel>
+                <ConLabel>
+                  <InputCon type="checkbox" />
+                  Figma
+                </ConLabel>
+                <ConLabel>
+                  <InputCon type="checkbox" />
+                  3D MAX
+                </ConLabel>
+                <ConLabel>
+                  <InputCon type="checkbox" />
+                  Blender
+                </ConLabel>
+              </MidContetWrap>
+            </RecMidContentWrap>
+          </RecruitWriteMidWrap>
+
           <RecTitleTextWrap>
-            <RecTitleText>구하는 직군</RecTitleText>
+            <RecTitleText>제목</RecTitleText>
+            <div>
+              <RecTitleTextInput />
+            </div>
           </RecTitleTextWrap>
-          <RecTopTextContentWrap>
-            <RecTopRadioLabel>
-              <RecTopRadio type="radio" name="RecRadio" />
-              프론트엔드 개발자
-            </RecTopRadioLabel>
-            <RecTopRadioLabel>
-              <RecTopRadio type="radio" name="RecRadio" />
-              백엔드 개발자
-            </RecTopRadioLabel>
-            <RecTopRadioLabel>
-              <RecTopRadio type="radio" name="RecRadio" />
-              그래픽 디자이너
-            </RecTopRadioLabel>
-          </RecTopTextContentWrap>
-        </RecruitWriteTopWrap>
 
-        <RecruitWriteMidWrap>
           <RecTitleTextWrap>
-            <RecTitleText>요구 스킬</RecTitleText>
+            <RecTitleText>프로젝트 제목</RecTitleText>
+            <div>
+              <RecTitleTextInput />
+            </div>
           </RecTitleTextWrap>
-          <RecMidContentWrap>
-            <MidTextWrap>
-              <MidText>개발자</MidText>
-            </MidTextWrap>
-            <MidContetWrap>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                React
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Vue.js
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                JavaScript
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Node.js
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Java
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Spring
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Python
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />C
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                C++
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                C#
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Swift
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Kotlin
-              </ConLabel>
-            </MidContetWrap>
-            <MidTextWrap>
-              <MidText>디자이너</MidText>
-            </MidTextWrap>
-            <MidContetWrap>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Illustrator
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                adobe XD
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Figma
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                3D MAX
-              </ConLabel>
-              <ConLabel>
-                <InputCon type="checkbox" />
-                Blender
-              </ConLabel>
-            </MidContetWrap>
-          </RecMidContentWrap>
-        </RecruitWriteMidWrap>
 
-        <RecTitleTextWrap>
-          <RecTitleText>제목</RecTitleText>
-          <div>
-            <RecTitleTextInput />
-          </div>
-        </RecTitleTextWrap>
-
-        <RecTitleTextWrap>
-          <RecTitleText>프로젝트 제목</RecTitleText>
-          <div>
-            <RecTitleTextInput />
-          </div>
-        </RecTitleTextWrap>
-
-        <RecTitleTextWrap>
-          <RecTitleText>프로젝트 기간</RecTitleText>
-          <div>
-            <DayPickerSub />
-          </div>
-        </RecTitleTextWrap>
-
-        <RecruitWriteTextBotWrap>
           <RecTitleTextWrap>
-            <RecTitleText>프로젝트 상세 설명</RecTitleText>
+            <RecTitleText>프로젝트 기간</RecTitleText>
+            <div>
+              <DayPickerSub />
+            </div>
           </RecTitleTextWrap>
-          <div>
-            <RecMainCon />
-          </div>
-        </RecruitWriteTextBotWrap>
 
-        <div>
-          <RecButtonWrap>
-            <RecButton>모집글 올리기</RecButton>
-          </RecButtonWrap>
-        </div>
+          <RecruitWriteTextBotWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>프로젝트 상세 설명</RecTitleText>
+            </RecTitleTextWrap>
+            <div>
+              <RecMainCon />
+            </div>
+          </RecruitWriteTextBotWrap>
+
+          <div>
+            <RecButtonWrap>
+              <RecButton type="submit">모집글 올리기</RecButton>
+            </RecButtonWrap>
+          </div>
+        </form>
       </RecruitWriteWrap>
     </>
   );
@@ -197,7 +292,6 @@ const RecTopRadio = styled.input`
   height: 15px;
   margin-bottom: -2px;
   margin-right: 5px;
-
   &:checked {
     border-color: transparent;
     background-color: #685bc7;
@@ -237,7 +331,6 @@ const InputCon = styled.input`
   height: 15px;
   margin-bottom: -3px;
   margin-right: 5px;
-
   &:checked {
     border-color: transparent;
     background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
