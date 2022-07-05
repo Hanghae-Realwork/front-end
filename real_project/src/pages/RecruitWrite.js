c02c5d91b533e232ee17333fa9472a5660a74109
kimjinhee <kjh11071@gmail.com>
2022-07-05 18:02:45 +0900

    폼훅 적용중

--- real_project/src/pages/RecruitWrite.js
+++ real_project/src/pages/RecruitWrite.js
@@ -1,144 +1,240 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import DayPickerSub from "../components/DayPickerSub";
+import { DayPicker } from "react-day-picker";
+import { format, isValid, parse, isAfter } from "date-fns";
+
+import { ko } from "date-fns/esm/locale";
 
 import Plus from "../image/plus.svg";
 
 function RecruitWrite() {
-    const dateref=useRef
-    const [selected, setSelected] = useState(dateref);
-    
-    const onSubmit = async (data) => {
-        await new Promise((r) => setTimeout(r, 1000));
-        alert(JSON.stringify(data));
-        console.log(data)
+  const dateref = useRef
+  const [selected, setSelected] = useState(new Date);
+  // const [startdate, setStart] = useState(selected.from);
+
+ 
+  const today = new Date();
+  const startdate = selected.from
+
+  const enddate = selected.to
+  
+
+
+  const onSubmit = async (data) => {
+    const output = {
+      ...data,
+      startdate: startdate,
+      enddate: enddate
+    }
+    await new Promise((r) => setTimeout(r, 1000));
+    alert(JSON.stringify(output));
+    console.log(data)
+    console.log(output)
+  }
+  
+  // console.log(selected)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isSubmitting, isDirty, errors }
+  } = useForm();
+
+  const addPost = async (e) => {
+
+
+  }
+
+  // const 
+  // const variables={
+  //   data:data,
+  //   date:selected
+  // }
+  // console.log(variables)
+
+  let footer = (
+    <p
+      style={{
+        margin: "10px",
+        padding: "10px",
+        border: "1px solid black",
+        borderRadius: "10px",
+        width: "180px",
+        textAlign: "center",
+        fontSize: "13px",
+      }}
+    >
+      시작날짜를 눌러주세요
+    </p>
+  );
+  if (selected?.from) {
+    if (!selected.to) {
+      footer = (
+
+        <p
+          style={{
+            margin: "10px",
+            padding: "10px",
+            border: "1px solid black",
+            borderRadius: "10px",
+            width: "150px",
+            textAlign: "center",
+            fontSize: "13px",
+          }}
+        >
+          {format(selected.from, "yyyy년 MM월 dd일")}
+
+        </p>
+
+      );
+    } else if (selected?.to) {
+      footer = (
+        <p
+          style={{
+            margin: "10px",
+            padding: "10px",
+            border: "1px solid black",
+            borderRadius: "10px",
+            width: "260px",
+            textAlign: "center",
+            fontSize: "13px",
+          }}
+        >
+          {format(selected.from, "yyyy년 MM월 dd일 ")}~
+          {format(selected.to, " yyyy년 MM월 dd일")}
+        </p>
+      );
     }
 
-    console.log(selected)
-    const {
-        register,
-        handleSubmit,
-        formState: { isSubmitting, isDirty, errors }
-    } = useForm();
+  }
+
+  return (
+    <>
+
+      <RecruitWriteWrap>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <RecruitWriteTopWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>구하는 직군</RecTitleText>
+            </RecTitleTextWrap>
+            <RecTopTextContentWrap >
+              <RecTopRadioLabel>
+                <RecTopRadio id="role" type="radio" name="RecRadio" value="frontend" {...register("role")} />프론트엔드 개발자
+              </RecTopRadioLabel>
+              <RecTopRadioLabel>
+                <RecTopRadio id="role" type="radio" name="RecRadio" value="backend" {...register("role")} />백엔드 개발자
+              </RecTopRadioLabel>
+              <RecTopRadioLabel>
+                <RecTopRadio id="role" type="radio" name="RecRadio" value="graphicDesigner" {...register("role")} />그래픽 디자이너
+              </RecTopRadioLabel>
+            </RecTopTextContentWrap>
+          </RecruitWriteTopWrap>
+
+          <RecruitWriteMidWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>요구 스킬</RecTitleText>
+            </RecTitleTextWrap>
+            <RecMidContentWrap>
+              <MidTextWrap>
+                <MidText>개발자</MidText>
+              </MidTextWrap>
+              <MidContetWrap>
+                <ConLabel><InputCon id="skills" type="checkbox" value="React" {...register("skills")} />React</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Vue.js" {...register("skills")} />Vue.js</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="JavaScript" {...register("skills")} />JavaScript</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Node.js" {...register("skills")} />Node.js</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Java" {...register("skills")} />Java</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Spring" {...register("skills")} />Spring</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Python" {...register("skills")} />Python</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="C" {...register("skills")} />C</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="C++" {...register("skills")} />C++</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="C#" {...register("skills")} />C#</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Swift" {...register("skills")} />Swift</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Kotlin" {...register("skills")} />Kotlin</ConLabel>
+              </MidContetWrap>
+              <MidTextWrap>
+                <MidText>디자이너</MidText>
+              </MidTextWrap>
+              <MidContetWrap>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Illustrator" {...register("skills")} />Illustrator</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="adobe XD" {...register("skills")} />adobe XD</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Figma" {...register("skills")} />Figma</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="3D MAX" {...register("skills")} />3D MAX</ConLabel>
+                <ConLabel><InputCon id="skills" type="checkbox" value="Blender" {...register("skills")} />Blender</ConLabel>
+              </MidContetWrap>
+            </RecMidContentWrap>
+          </RecruitWriteMidWrap>
+
+          <RecTitleTextWrap>
+            <RecTitleText htmlFor="title">제목</RecTitleText>
+            <div>
+
+              <RecTitleTextInput
+                id="title"
+                type="text"
+                placeholder="제목을 입력해주세요"
+                {...register("title")}
+              />
+            </div>
+          </RecTitleTextWrap>
+
+          <RecTitleTextWrap>
+            <RecTitleText htmlFor="subscript">프로젝트 제목</RecTitleText>
+            <div><RecTitleTextInput
+              id="subscript"
+              type="text"
+              placeholder="프로젝트의 제목을 입력해주세요"
+              {...register("subscript")}
+            /></div>
+          </RecTitleTextWrap>
+
+          <RecTitleTextWrap>
+            <RecTitleText>프로젝트 기간</RecTitleText>
+            <div id="DayPicker" >
+              <DayPicker
+                styles={{
+                  caption: { fontSize: "10px", padding: "10px" },
+                }}
+                
+
+                className="dayPicker_container__div"
+                mode="range"
+                selected={selected}
+
+                onSelect={setSelected}
+                locale={ko}
+                numberOfMonths={2}
+                disabled={{ before: today }}
+                
+              ></DayPicker>
+              {footer}
+            </div>
+          </RecTitleTextWrap>
 
 
-    return (
-        <>
+          <RecruitWriteTextBotWrap>
+            <RecTitleTextWrap>
+              <RecTitleText>프로젝트 상세 설명</RecTitleText>
+            </RecTitleTextWrap>
+            <div><RecMainCon
+              id="details"
+              type="text"
+              placeholder="프로젝트의 내용을 입력해주세요"
+              {...register("details")}
+            /></div>
+          </RecruitWriteTextBotWrap>
+          <div>
+            <RecButtonWrap >
+              <RecButton type="submit" disabled={isSubmitting} onClick={addPost}>모집글 올리기</RecButton>
+            </RecButtonWrap>
+          </div>
+        </form>
+      </RecruitWriteWrap>
 
-            <RecruitWriteWrap>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <RecruitWriteTopWrap>
-                        <RecTitleTextWrap>
-                            <RecTitleText>구하는 직군</RecTitleText>
-                        </RecTitleTextWrap>
-                        <RecTopTextContentWrap >
-                            <RecTopRadioLabel>
-                                <RecTopRadio id="role" type="radio" name="RecRadio" value="frontend" {...register("role")} />프론트엔드 개발자
-                            </RecTopRadioLabel>
-                            <RecTopRadioLabel>
-                                <RecTopRadio id="role" type="radio" name="RecRadio" value="backend" {...register("role")} />백엔드 개발자
-                            </RecTopRadioLabel>
-                            <RecTopRadioLabel>
-                                <RecTopRadio id="role" type="radio" name="RecRadio" value="graphicDesigner" {...register("role")} />그래픽 디자이너
-                            </RecTopRadioLabel>
-                        </RecTopTextContentWrap>
-                    </RecruitWriteTopWrap>
-
-                    <RecruitWriteMidWrap>
-                        <RecTitleTextWrap>
-                            <RecTitleText>요구 스킬</RecTitleText>
-                        </RecTitleTextWrap>
-                        <RecMidContentWrap>
-                            <MidTextWrap>
-                                <MidText>개발자</MidText>
-                            </MidTextWrap>
-                            <MidContetWrap>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="React" {...register("skills")} />React</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Vue.js" {...register("skills")} />Vue.js</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="JavaScript" {...register("skills")} />JavaScript</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Node.js" {...register("skills")} />Node.js</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Java" {...register("skills")} />Java</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Spring" {...register("skills")} />Spring</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Python" {...register("skills")} />Python</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="C" {...register("skills")} />C</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="C++" {...register("skills")} />C++</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="C#" {...register("skills")} />C#</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Swift" {...register("skills")} />Swift</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Kotlin" {...register("skills")} />Kotlin</ConLabel>
-                            </MidContetWrap>
-                            <MidTextWrap>
-                                <MidText>디자이너</MidText>
-                            </MidTextWrap>
-                            <MidContetWrap>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Illustrator" {...register("skills")} />Illustrator</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="adobe XD" {...register("skills")} />adobe XD</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Figma" {...register("skills")} />Figma</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="3D MAX" {...register("skills")} />3D MAX</ConLabel>
-                                <ConLabel><InputCon id="skills" type="checkbox" value="Blender" {...register("skills")} />Blender</ConLabel>
-                            </MidContetWrap>
-                        </RecMidContentWrap>
-                    </RecruitWriteMidWrap>
-
-                    <RecTitleTextWrap>
-                        <RecTitleText htmlFor="title">제목</RecTitleText>
-                        <div>
-
-                            <RecTitleTextInput
-                                id="title"
-                                type="text"
-                                placeholder="제목을 입력해주세요"
-                                {...register("title")}
-                            />
-                        </div>
-                    </RecTitleTextWrap>
-
-                    <RecTitleTextWrap>
-                        <RecTitleText htmlFor="subscript">프로젝트 제목</RecTitleText>
-                        <div><RecTitleTextInput
-                            id="subscript"
-                            type="text"
-                            placeholder="프로젝트의 제목을 입력해주세요"
-                            {...register("subscript")}
-                        /></div>
-                    </RecTitleTextWrap>
-
-                    <RecTitleTextWrap>
-                        <RecTitleText>프로젝트 기간</RecTitleText>
-                        <div>
-                            <DayPickerSub
-                                id="selected"
-                                selected={selected}
-                                type="date"
-                                value="selected"
-                                dateref={dateref}
-                                {...register("selected")}
-                            />
-                        </div>
-                    </RecTitleTextWrap>
-
-
-                    <RecruitWriteTextBotWrap>
-                        <RecTitleTextWrap>
-                            <RecTitleText>프로젝트 상세 설명</RecTitleText>
-                        </RecTitleTextWrap>
-                        <div><RecMainCon
-                            id="details"
-                            type="text"
-                            placeholder="프로젝트의 내용을 입력해주세요"
-                            {...register("details")}
-                        /></div>
-                    </RecruitWriteTextBotWrap>
-                    <div>
-                        <RecButtonWrap >
-                            <RecButton type="submit" disabled={isSubmitting}>모집글 올리기</RecButton>
-                        </RecButtonWrap>
-                    </div>
-                </form>
-            </RecruitWriteWrap>
-
-        </>
-    );
+    </>
+  );
 }
 
 const RecruitWriteWrap = styled.div`
