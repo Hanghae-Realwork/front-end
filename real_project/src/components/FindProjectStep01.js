3a89dd76cdd3c8d408328f14a99886ac643066a6
kimjinhee <kjh11071@gmail.com>
2022-07-09 15:54:24 +0900

    step 수정/등록 성공

--- real_project/src/components/FindProjectStep01.js
+++ real_project/src/components/FindProjectStep01.js
@@ -2,10 +2,8 @@ import { useNavigate } from "react-router-dom";
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import axios from "axios";
-
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-
 import { useForm, useWatch } from "react-hook-form";
 import DayPickerSub from "../components/DayPickerSub";
 import { DayPicker } from "react-day-picker";
@@ -13,42 +11,32 @@ import { format, isValid, parse, isAfter } from "date-fns";
 import "react-day-picker/dist/style.css";
 import "../components/day-picker.css";
 import { ko } from "date-fns/esm/locale";
-
 import FindStep01 from "../components/FindProjectStep01"
 import FindStep02 from "../components/FindProjectStep02"
-
 import SelectSkill from "../components/SelectSkill";
-
 import { createRecruitApi } from "../redux/modules/postRecruit";
-
 
 const FindProjectStep01 = (props) => {
   const dispatch = useDispatch
-
-
   const navigate = useNavigate()
-
   const dateref = useRef
   const [selected, setSelected] = useState(new Date);
   // const [startdate, setStart] = useState(selected.from);
   const today = new Date();
   const startdate = JSON.stringify(selected.from)
   const enddate = JSON.stringify(selected.to)
-
+  const delStorage = localStorage.removeItem('obj')
   const onSubmit = async (data) => {
     const output = {
       ...data,
-
-
       start: startdate.slice(1, 11),
       end: enddate.slice(1, 11),
-
       photos: ["null", "null"],
       schedule: ["2022-07-20", "2022-07-25"]
     }
-
-    // await new Promise((r) => setTimeout(r, 1000));
-    localStorage.setItem('obj', JSON.stringify(output))
+    await new Promise((r) => setTimeout(r, 1000));
+    
+    sessionStorage.setItem('obj', JSON.stringify(output))
     // dispatch(
     //   createRecruitApi({
     //     ...output
@@ -56,15 +44,12 @@ const FindProjectStep01 = (props) => {
     // );
     console.log(output)
     navigate(`/findprojectstep2`)
-    
-
+    // await new Promise((delStorage) => setTimeout(delStorage,  10000));
   }
-
   const {
     register, handleSubmit,
     formState: { isSubmitting, isDirty, errors },
   } = useForm();
-
   let footer = (
     <Ptag>시작날짜를 눌러주세요</Ptag>
   );
@@ -84,11 +69,9 @@ const FindProjectStep01 = (props) => {
       );
     }
   }
-
   return (
     <>
       <FindProjectAllWrap>
-
         <form onSubmit={handleSubmit(onSubmit)}>
           <FindprojectTopWrap>
             <FindProjectTitleText>새로운 크루 모집하기</FindProjectTitleText>
@@ -117,14 +100,11 @@ const FindProjectStep01 = (props) => {
               {...register("subscript")}
             ></ProjectInput>
           </FindProjectInputTitle>
-
           <FindProjectInputDate>
             <ProjectTitleText>프로젝트 기간</ProjectTitleText>
             <div>
               <DayPicker
-
                 styles={{ caption: { fontSize: "13px", padding: "10px" } }}
-
                 className="dayPicker_container__div"
                 mode="range"
                 selected={selected}
@@ -135,7 +115,6 @@ const FindProjectStep01 = (props) => {
               ></DayPicker>
               {footer}
             </div>
-
           </FindProjectInputDate>
           <FindProjectInputTitle>
             <ProjectTitleText>팀 상세 설명</ProjectTitleText>
@@ -145,12 +124,10 @@ const FindProjectStep01 = (props) => {
           // onClick={() => {navigate(`/findprojectstep2`)}}
           >다음 단계로</button></div>
         </form>
-
       </FindProjectAllWrap>
     </>
   );
 };
-
 
 const FindProjectAllWrap = styled.div`
   /* border: 1px solid black; */
@@ -160,13 +137,11 @@ const FindProjectAllWrap = styled.div`
   align-items: flex-start;
 `
 
-
 const FindProjectTitleText = styled.span`
   font-size: 20px;
   font-weight: 700;
   margin: 30px 0px 30px 32px;
 `
-
 const FindprojectTopWrap = styled.div`
   height: 90px;
   display: flex;
@@ -174,63 +149,31 @@ const FindprojectTopWrap = styled.div`
   justify-content: flex-start;
   align-items: center;
 `
-
 const FindProjectStepGuideText1 = styled.span`
   font-size: 18px;
   font-weight: 500;
   margin: 17px 0px 23px 30px;
 `
-
 const FindProjectStepGuideText2 = styled.span`
   font-size: 18px;
   font-weight: 500;
   margin: 17px 0px 23px 30px;
 `
-
 const FindProjectStepWrap = styled.div`
   height: 67px;
-
   display: flex;
   flex-flow: row wrap;
   justify-content: flex-start;
   align-items: center;
-
   gap: 15px;
 `
-
 const HeadLine = styled.hr`
   border: 1px solid #D9D9D9;
   width: 1200px;
 `
 
-const FindProjectInputTitle = styled.div`
-  /* border: 1px solid black; */
-  margin: 40px 0px 16px 30px;
-  width: 1100px;
 
-`
 
-const FindProjectStepGuideText1 = styled.span`
-  font-size: 18px;
-  font-weight: 500;
-  margin: 17px 0px 23px 30px;
-`
-
-const FindProjectStepGuideText2 = styled.span`
-  font-size: 18px;
-  font-weight: 500;
-  margin: 17px 0px 23px 30px;
-`
-
-const FindProjectStepWrap = styled.div`
-  height: 67px;
-
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: flex-start;
-  align-items: center;
-
-`
 
 const FindProjectInputDate = styled.div`
   margin: 40px 0px 16px 30px;
@@ -240,21 +183,12 @@ const FindProjectInputDate = styled.div`
   justify-content: center;
   align-items: flex-start;
 `
-
 const ProjectTitleText = styled.span`
   font-size: 16px;
   font-weight: 500;
-  
-`
-
-
   gap: 15px;
 `
 
-const HeadLine = styled.hr`
-  border: 1px solid #D9D9D9;
-  width: 1200px;
-`
 
 const FindProjectInputTitle = styled.div`
   /* border: 1px solid black; */
@@ -266,20 +200,7 @@ const FindProjectInputTitle = styled.div`
   align-items: center;
 `
 
-const FindProjectInputDate = styled.div`
-  margin: 40px 0px 16px 30px;
-  width: 1100px;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: flex-start;
-`
 
-const ProjectTitleText = styled.span`
-  font-size: 16px;
-  font-weight: 500;
-  
-`
 
 const ProjectInput = styled.input`
   border: none;
@@ -289,7 +210,6 @@ const ProjectInput = styled.input`
   width: 1000px;
   margin-top: 16px;
 `
-
 const Ptag = styled.p`
   margin: 10px; 
   padding: 10px; 
@@ -300,14 +220,12 @@ const Ptag = styled.p`
   font-size: 13px;
   font-weight: 500;
 `
-
 const TextAreaWrap = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   align-items: flex-start;
 `
-
 const RecMainCon = styled.textarea`
   margin: 20px; 
   padding: 10px; 
@@ -316,9 +234,6 @@ const RecMainCon = styled.textarea`
   outline: none; 
   resize: none;
 `;
-
 const NextStepButton = styled.button`
-
 `
-
 export default FindProjectStep01;
