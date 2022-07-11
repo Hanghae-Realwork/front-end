d5c118f5ef13ae11157d57060e0f02b6db39795b
kimjinhee <kjh11071@gmail.com>
2022-07-11 17:37:48 +0900

    step1 세션스토리지 로드 적용

--- real_project/src/components/FindProjectStep01.js
+++ real_project/src/components/FindProjectStep01.js
@@ -20,30 +20,32 @@ const FindProjectStep01 = (props) => {
   const dispatch = useDispatch
   const navigate = useNavigate()
   const dateref = useRef
+  const callStorage = sessionStorage.getItem('obj')
+  const storageData = JSON.parse(callStorage)
   const [selected, setSelected] = useState(new Date);
   // const [startdate, setStart] = useState(selected.from);
   const today = new Date();
   const startdate = JSON.stringify(selected.from)
   const enddate = JSON.stringify(selected.to)
-  const delStorage = localStorage.removeItem('obj')
+
+  console.log(storageData)
+
   const onSubmit = async (data) => {
+
     const output = {
       ...data,
       start: startdate.slice(1, 11),
       end: enddate.slice(1, 11),
+      selected:selected,
       photos: ["null", "null"],
-      
+
     }
     await new Promise((r) => setTimeout(r, 1000));
-    
+
     sessionStorage.setItem('obj', JSON.stringify(output))
-    // dispatch(
-    //   createRecruitApi({
-    //     ...output
-    //   })
-    // );
+
     console.log(output)
-    navigate(`/findprojectstep2`)
+    navigate("/findprojectstep2")
     // await new Promise((delStorage) => setTimeout(delStorage,  10000));
   }
   const {
@@ -84,25 +86,47 @@ const FindProjectStep01 = (props) => {
           <HeadLine />
           <FindProjectInputTitle>
             <ProjectTitleText>제목 (최대 n자 이내)</ProjectTitleText>
-            <ProjectInput
-              id="title"
-              type="text"
-              placeholder="제목을 입력해주세요"
-              {...register("title")}
-            ></ProjectInput>
+            {storageData ? (
+              <ProjectInput
+                id="title"
+                type="text"
+                placeholder="제목을 입력해주세요"
+                value={storageData.title}
+                {...register("title", { required: true })}
+              ></ProjectInput>
+            ) : (
+              <ProjectInput
+                id="title"
+                type="text"
+                placeholder="제목을 입력해주세요"
+                {...register("title", { required: true })}
+              ></ProjectInput>
+            )}
           </FindProjectInputTitle>
+
           <FindProjectInputTitle>
             <ProjectTitleText>프로젝트 설명 (최대 n자 이내)</ProjectTitleText>
-            <ProjectInput
-              id="subscript"
-              type="text"
-              placeholder="프로젝트를 설명해주세요"
-              {...register("subscript")}
-            ></ProjectInput>
+            {storageData ? (
+              <ProjectInput
+                id="subscript"
+                type="text"
+                placeholder="프로젝트를 설명해주세요"
+                value={storageData.subscript}
+                {...register("subscript", { required: true })}
+              ></ProjectInput>
+            ) : (
+              <ProjectInput
+                id="subscript"
+                type="text"
+                placeholder="프로젝트를 설명해주세요"
+                {...register("subscript", { required: true })}
+              ></ProjectInput>
+            )}
           </FindProjectInputTitle>
           <FindProjectInputDate>
             <ProjectTitleText>프로젝트 기간</ProjectTitleText>
             <div>
+            {storageData ? (
               <DayPicker
                 styles={{ caption: { fontSize: "13px", padding: "10px" } }}
                 className="dayPicker_container__div"
@@ -112,22 +136,52 @@ const FindProjectStep01 = (props) => {
                 locale={ko}
                 numberOfMonths={2}
                 disabled={{ before: today }}
+                value={storageData.selected}
               ></DayPicker>
+              ) : (
+                <DayPicker
+                styles={{ caption: { fontSize: "13px", padding: "10px" } }}
+                className="dayPicker_container__div"
+                mode="range"
+                selected={selected}
+                onSelect={setSelected}
+                locale={ko}
+                numberOfMonths={2}
+                disabled={{ before: today }}
+              ></DayPicker>
+              )}
               {footer}
-            </div>
+              </div>
+            
           </FindProjectInputDate>
           <FindProjectInputTitle>
             <ProjectTitleText>팀 상세 설명</ProjectTitleText>
-            <div><RecMainCon id="details" type="text" placeholder="프로젝트의 내용을 입력해주세요" {...register("details")} /></div>
+            <div>
+            {storageData ? (
+              <RecMainCon
+                id="details"
+                type="text"
+                placeholder="프로젝트의 내용을 입력해주세요"
+                value={storageData.details}
+                {...register("details")}
+              />
+              ) : (
+                <RecMainCon
+                id="details"
+                type="text"
+                placeholder="프로젝트의 내용을 입력해주세요"
+                {...register("details")}
+              />
+              )}
+            </div>
           </FindProjectInputTitle>
-          <div><button type="submit" disabled={isSubmitting}
-          // onClick={() => {navigate(`/findprojectstep2`)}}
-          >다음 단계로</button></div>
+          <div><NextStepButton type="submit" disabled={isSubmitting} >다음 단계로</NextStepButton></div>
         </form>
       </FindProjectAllWrap>
     </>
   );
 };
+
 
 const FindProjectAllWrap = styled.div`
   /* border: 1px solid black; */
@@ -171,7 +225,6 @@ const HeadLine = styled.hr`
   border: 1px solid #D9D9D9;
   width: 1200px;
 `
-
 
 const FindProjectInputDate = styled.div`
   margin: 40px 0px 16px 30px;
@@ -226,11 +279,23 @@ const TextAreaWrap = styled.div`
 const RecMainCon = styled.textarea`
   margin: 20px; 
   padding: 10px; 
-  width: 1000px; 
+  width: 1100px; 
   height: 500px; 
   outline: none; 
   resize: none;
+  border-radius: 4px;
 `;
 const NextStepButton = styled.button`
+  width: 150px;
+  height: 45px;
+  background: linear-gradient(115.2deg, #AE97E3 0%, #77C3E7 77.66%);
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  margin: 30px 0px 30px 0px;
+  padding: 12px 28px;
+  color: white;
+  font-weight: 700;
 `
 export default FindProjectStep01;
