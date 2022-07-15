99bc5309e3d141c51d5cd1abc140d2bc05ae841a
Frost <tolerate0407@gmail.com>
2022-07-16 01:00:49 +0900

    add: miniresume 추가

--- real_project/src/App.js
+++ real_project/src/App.js
@@ -10,12 +10,9 @@ import Join from "./pages/Join";
 import Login from "./pages/Login";
 import MainEmployment from "./pages/MainEmployment";
 import MainRecruit from "./pages/MainRecruit";
-// import RecruitWrite from "./pages/RecruitWrite";
 import Main from "./pages/Main";
 import MyPage from "./pages/MyPage"
-// import ChatJoin from "./pages/ChatPassword"
 import ReadProject from "./pages/ReadProject"
-
 import Application from "./components/Mypage/Application";
 import Recruitment from "./components/Mypage/Recruitment";
 import Resumes from "./components/Mypage/Resumes";
@@ -33,6 +30,7 @@ import { checkUserValidation } from "./redux/modules/user";
 
 
 
+
 function App() {
   
 const dispatch = useDispatch()
@@ -44,46 +42,19 @@ const dispatch = useDispatch()
   return (
     <MainWrap>
       <Header />
-      {/* <DatePickerTest/> */}
-      {/* <ReadProject></ReadProject> */}
-      {/* <ChatJoin /> */}
-      {/* <MyPage /> */}
-      {/* <EmpoCard></EmpoCard> */}
-      {/* </MainHeader> */}
-      {/* <AddProfile/> */}
-      {/* <Test/> */}
-      {/* <Chat/> */}
-      {/* <EmploymentProfile/> '완료' */}
-      {/* <Join/> '완료' */}
-      {/* <Login/> '완료' */}
-      {/* <MainEmployment/>
-      <MainRecruit/> */}
-      {/* <FindProject/>  - 해결중 */}
-      {/* <FindProjectStep02 /> */}
-      {/* <RecruitWrite/> '완료' */}
-      {/* <Loading/> '완료' */}
-      {/* <CardRecruit/> '컴포넌트' */}
-      {/* <SelectBox/> '컴포넌트' */}
-
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mainrecruit" element={<MainRecruit />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
-        <Route
-          path="/employmentprofile/:resumeId"
-          element={<EmploymentProfile />}
-        />
+        <Route path="/employmentprofile/:resumeId" element={<EmploymentProfile />}/>
         <Route path="/addprofile" element={<AddProfile />} />
         <Route path="/editprofile/:resumeId" element={<EditProfile />} />
         <Route path="/mainemployment" element={<MainEmployment />}></Route>
 
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/findprojectstep1" element={<FindProjectStep01 />}></Route>
-        <Route
-          path="/findprojectstep2/:projectId"
-          element={<FindProjectStep02 />}
-        ></Route>
+        <Route path="/findprojectstep2/:projectId" element={<FindProjectStep02 />}></Route>
         <Route path="/readproject/:projectId" element={<ReadProject />}></Route>
 
         <Route path="mypage/:nickname/*" element={<MyPage />}>
---
+++ real_project/src/components/MiniResume.js
@@ -0,0 +1,149 @@
+import React, { useEffect,useState } from "react";
+import styled from "styled-components";
+import { useSelector,useDispatch } from "react-redux";
+import { deleteEmployAxios, loadSingleEmployAxios } from "../redux/modules/postEmploy";
+
+import TagCompo from "./TagCompoEmp";
+
+import astroman from "../image/astroman.svg"
+
+
+
+
+function MiniResume() {
+
+    const dispatch = useDispatch();
+
+    const data = useSelector((state) => state.postEmploy.resumes);
+    console.log(data)
+
+
+  return (
+    <>
+      <MiniCardAllWrap>
+        <MiniTopWrap>
+            <MiniPhotoWrap>
+                <Photo></Photo>
+            </MiniPhotoWrap>
+            <MiniNameWrap>
+                <MiniNickName>닉네임이 들어갑니다</MiniNickName>
+                <MiniRole>직군이 들어갑니다</MiniRole>
+            </MiniNameWrap>
+        </MiniTopWrap>
+
+        <MiniBodyWrap>
+            <MiniBodyText>내용이 들어갑니다</MiniBodyText>
+        </MiniBodyWrap>
+
+        <MiniDateWrap>
+            <span style={{fontSize:"12px"}}>프로젝트 가능 기간</span> <MiniDateText></MiniDateText>
+        </MiniDateWrap>
+
+        <TecWrap>
+            보유한 기술
+            <TecMiniWrap>
+                <TagCompo/>
+            </TecMiniWrap>
+        </TecWrap>
+      </MiniCardAllWrap>
+    </>
+  );
+}
+
+
+const MiniCardAllWrap = styled.div`
+    border: 1px solid black;
+    border-radius: 4px;
+    width: 384px;
+    height: 250px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: flex-start;
+`
+
+const MiniTopWrap = styled.div`
+    /* border: 1px solid black; */
+    width: 344px;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 15px 0px 12px 20px;
+`
+
+const MiniBodyWrap = styled.div`
+    /* border: 1px solid black; */
+    width: 344px;
+    margin: 0px 12px 12px 20px;
+`
+
+const MiniDateWrap = styled.div`
+    /* border: 1px solid black; */
+    width: 344px;
+    margin: 0px 0px 16px 20px;
+`
+
+const TecWrap = styled.div`
+    /* border: 1px solid black; */
+    width: 344px;
+    margin: 0px 0px 21px 20px;
+`
+
+const MiniPhotoWrap = styled.div`
+    width: 60px;
+    height: 60px;
+`
+
+const Photo = styled.div`
+    /* border: 1px solid black; */
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+    background-image: url(${astroman});
+    background-size: cover;
+    background-position: center;
+`
+
+const MiniNickName = styled.span`
+    font-size: 18px;
+    font-weight: 700;
+`
+
+const MiniNameWrap = styled.div`
+    /* border: 1px solid black; */
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: flex-start;
+    margin-left: 12px;
+`
+
+const MiniRole = styled.span`
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const MiniBodyText = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+`
+
+const MiniDateText = styled.span`
+    font-size: 12px;
+    font-weight: 500;
+`
+
+const TecMiniWrap = styled.div`
+    /* bordeR: 1px solid black; */
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: center;
+    width: 344px;
+`
+
+
+
+
+export default MiniResume;
