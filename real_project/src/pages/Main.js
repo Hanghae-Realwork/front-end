80f1bb26604342540c105af608ae6946f2038061
Frost <tolerate0407@gmail.com>
2022-07-29 19:24:51 +0900

    edit: 오류 수정

--- real_project/src/pages/Main.js
+++ real_project/src/pages/Main.js
@@ -24,7 +24,7 @@ import planet from "../image/rotationplanet.svg"
 function Main() {
 
     const navigate = useNavigate();
-    console.log('빌드 1.0.188')
+    console.log('빌드 1.0.191')
 
     return(
         <>
